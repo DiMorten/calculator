7ee553a773414503c01dd0f4a1bc0c9422c300f7
DiMorten <cmjorgeandres@gmail.com>
2022-02-20 00:38:31 -0500

    Add button grid 2

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 
 const buttonContainer = document.querySelector('#button-container');
+const arrowsContainer = document.querySelector('#arrows-container');
 
-function createGrid(container, gridSize) {
+class GridProperties {
+    constructor(container, text, size) {
+        this.container = container;
+        this.text = text;
+        this.size = size;
+    }
+}
+
+function createGrid(gridProperties) {
     
-    for (i=0; i<gridSize; i++) {
+    for (i=0; i<gridProperties.size[0]; i++) {
         const rowDiv = document.createElement('div');
         rowDiv.setAttribute('class', 'row');
-        container.appendChild(rowDiv);
-        for (j=0; j<gridSize; j++) {
-            const boxDiv = document.createElement('div');
-            boxDiv.setAttribute('class', 'box');
+        gridProperties.container.appendChild(rowDiv);
+        for (j=0; j<gridProperties.size[1]; j++) {
+            text = gridProperties.text[i][j];
+            const boxDiv = document.createElement('button');
             boxDiv.setAttribute('id', 'box_' + j.toString() + '_' + i.toString());
-            
-            // boxDiv.addEventListener('mouseover', changeColor)
-            // boxDiv.addEventListener('mousedown', changeColor)
+            boxDiv.setAttribute('data-key', text);
+            if (text === ' ') boxClass = 'hidden-box'
+            else {
+                boxClass = 'box';
+                boxDiv.innerHTML = text;
+            }
+            boxDiv.setAttribute('class', 'box');
+            console.log(gridProperties);
 
             rowDiv.appendChild(boxDiv);        
         }
@@ -21,4 +35,23 @@ function createGrid(container, gridSize) {
 
 }
 
-createGrid(buttonContainer, 5);
+arrowsGridText = [[' ', '↥', ' '],
+                ['↤', ' ', '↦'],
+                [' ', '↧', ' ']];
+
+arrowsGridProperties = new GridProperties(arrowsContainer,
+        arrowsGridText,
+        [3, 3])
+
+createGrid(arrowsGridProperties);
+
+buttonGridText = [[' ', ' ', 'bck', 'b'],
+                ['7', '8', '9', '*'], 
+                ['4', '5', '6', '-'], 
+                ['1', '2', '3', '+'], 
+                ['0', '.', 'clear', 'enter']]
+
+buttonGridProperties = new GridProperties(buttonContainer,
+        buttonGridText,
+        [5, 4])
+createGrid(buttonGridProperties);
