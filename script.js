692f3e4adf10d6c7b765b59b7fbf7cac6dbefc8c
DiMorten <cmjorgeandres@gmail.com>
2022-02-20 18:32:55 -0500

    Calculate operations

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 const buttonContainer = document.querySelector('#button-container');
 const arrowsContainer = document.querySelector('#arrows-container');
 
+const displayInput = document.querySelector('#display-input');
+const displayHistory = document.querySelector('#display-history');
+
+const DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const OPERATIONS = ['*', '+', '-', '/']
+
 class GridProperties {
     constructor(container, text, size) {
         this.container = container;
@@ -21,18 +27,107 @@ function createGrid(gridProperties) {
             const boxDiv = document.createElement('button');
             boxDiv.setAttribute('id', 'box_' + j.toString() + '_' + i.toString());
             boxDiv.setAttribute('data-key', text);
-            if (text === ' ') boxClass = 'hidden-box'
+            if (text === ' ') boxClass = 'box'
             else {
                 boxClass = 'box';
                 boxDiv.innerHTML = text;
             }
-            boxDiv.setAttribute('class', 'box');
-            console.log(gridProperties);
 
-            rowDiv.appendChild(boxDiv);        
+            boxDiv.setAttribute('class', boxClass);
+            // console.log(gridProperties);
+
+
+            if (DIGITS.includes(text)) callback = digitsCallback;
+            else if (OPERATIONS.includes(text)) callback = operationsCallback;
+            else callback = otherButtonsCallback;
+            boxDiv.addEventListener('mousedown', callback);   
+            rowDiv.appendChild(boxDiv);     
+
         }
     }
+}
 
+function digitsCallback(e) {
+    const key = e.target.dataset.key;
+    displayInput.innerHTML += key;
+    // displayInputString += e.
+
+}
+
+function operationsCallback(e) {
+    const key = e.target.dataset.key;
+    displayInput.innerHTML += key;
+
+    // console.log(e);
+    // displayInputString += e.
+
+}
+
+function otherButtonsCallback(e) {
+    const key = e.target.dataset.key;
+    if (key === 'bck') 
+        displayInput.innerHTML = displayInput.innerHTML.slice(0, -1);
+    else if (key === 'enter') {
+        calculateFromString(displayInput.innerHTML);
+    }
+    else if (key === 'clear') {
+        displayInput.innerHTML = '';
+    }
+
+    console.log(e);
+    // displayInputString += e.
+
+}
+
+function calculateFromString(string) {
+    /* for (operation in OPERATIONS) {
+        string = [string.split(operation);
+    }
+    console.log({string}); */
+    indexOperator = false;
+    indexEndOperation = false;
+    partialResult = 0;
+    for (i = 0; i< string.length; i++) {
+        char = string.charAt(i);
+        if (!DIGITS.includes(char))   {
+            if (indexOperator === false) {
+                operationType = char;
+                indexOperator = i;    
+                operation = char;
+                a = string.slice(0, indexOperator);
+                console.log({indexOperator});
+                console.log({a});
+                console.log({operation});
+            }
+            else {
+
+                indexEndOperation = i;
+                b = string.slice(indexOperator + 1, indexEndOperation);
+                console.log({b});
+
+            }
+        }
+        else if (i === string.length - 1) {
+            indexEndOperation = i;
+            // console.log(indexEndOperation, string, indexOperator + 1, indexEndOperation)
+            b = string.slice(indexOperator + 1, indexEndOperation + 1);
+            // console.log({b});
+        }            
+    }
+    result = operationFromString(parseInt(a), parseInt(b), operation);
+    console.log({result});
+    displayHistory.innerHTML = result;
+
+}
+
+function operationFromString(a, b, operation) {
+    switch (operation) {
+        case '+': return a + b;  
+        case '-': return a - b;  
+        case '*': return a * b;  
+        case '/': return a / b;  
+        default: return "ERROR";    
+    }
 }
 
 arrowsGridText = [[' ', '↥', ' '],
@@ -45,7 +140,7 @@ arrowsGridProperties = new GridProperties(arrowsContainer,
 
 createGrid(arrowsGridProperties);
 
-buttonGridText = [[' ', ' ', 'bck', 'b'],
+buttonGridText = [[' ', ' ', 'bck', '/'],
                 ['7', '8', '9', '*'], 
                 ['4', '5', '6', '-'], 
                 ['1', '2', '3', '+'], 
